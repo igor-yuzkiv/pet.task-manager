77a75468d54af011c9d001c3dca409024fd269ad
Igor Yuzkiv <igor97w@gmail.com>
2025-02-09 16:20:59 +0200

    feature/projects-crud

--- resources/js/pages/project-detail/ProjectDetailPage.vue
+++ resources/js/pages/project-detail/ProjectDetailPage.vue
@@ -7,18 +7,18 @@ import { AppRouters } from '@/app/router/app-router.ts'
 
 const route = useRoute()
 const navItems = [
-    { title: 'Overview', route: AppRouters.projectOverview },
-    { title: 'Tasks', route: AppRouters.projectTasks },
-    { title: 'Documents', route: AppRouters.projectDocuments },
+    { title: 'Overview', route: AppRouters.projectOverview.name },
+    { title: 'Tasks', route: AppRouters.projectTasks.name },
+    { title: 'Documents', route: AppRouters.projectDocuments.name },
 ]
 </script>
 
 <template>
     <div class="flex flex-grow flex-col">
-        <Tabs :value="route.name">
+        <Tabs :value="route.name as string">
             <TabList>
-                <router-link v-for="navItem in navItems" :key="navItem.title" :to="{ name: navItem.route.name }">
-                    <Tab :value="navItem.route.name" class="px-3 py-1">{{ navItem.title }}</Tab>
+                <router-link v-for="navItem in navItems" :key="navItem.title" :to="{ name: navItem.route }">
+                    <Tab :value="navItem.route" class="px-3 py-1">{{ navItem.title }}</Tab>
                 </router-link>
             </TabList>
         </Tabs>
